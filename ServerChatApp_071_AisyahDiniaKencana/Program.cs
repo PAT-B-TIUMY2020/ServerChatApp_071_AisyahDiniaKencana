75f524545dfcd65da926c6da5fda66087ee39fc3
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2021-01-13 11:19:18 +0700

    server

--- ServerChatApp_071_AisyahDiniaKencana/Program.cs
+++ ServerChatApp_071_AisyahDiniaKencana/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WCFService_2Way_071;
 
 namespace ServerChatApp_071_AisyahDiniaKencana
 {
@@ -10,6 +12,23 @@ namespace ServerChatApp_071_AisyahDiniaKencana
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
         }
     }
 }
